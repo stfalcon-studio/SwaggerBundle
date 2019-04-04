b2991eab7f28061105191d3db388dafa23a66808
Yevhenii Vaskevych <evhenii.vaskevych@stfalcon.com>
2019-04-05 00:09:11 +0300

    Removed depth for scan files in folder

--- src/Config/ConfigParser.php
+++ src/Config/ConfigParser.php
@@ -80,7 +80,6 @@ class ConfigParser
     {
         $finder = new Finder();
         $finder
-            ->depth('<=1')
             ->files()
             ->in($dirPath)
             ->sortByName()
