0837e2fc32cc87ea6d2dd664a862e356283f69ea
Artem Henvald <artem.genvald@stfalcon.com>
2022-07-28 15:52:05 +0300

    Switch to php config

---
+++ Resources/config/services.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the SwaggerBundle.
+ *
+ * (c) Stfalcon LLC <stfalcon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->bind('$configFolder', '%swagger.config_folder%')
+        ->bind('$docsFolder', '%kernel.project_dir%/public/api/');
+
+    $services->load('StfalconStudio\SwaggerBundle\\', __DIR__ . '/../../{Command,Config,Generator}');
+};
